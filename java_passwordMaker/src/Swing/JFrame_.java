55f120ee9a0cf4254169a03713edccb855e87607
Fengli_ruomeng <72193590+Fengli-ruomeng@users.noreply.github.com>
2021-02-09 10:58:32 -0600

    Add files via upload

---
+++ java_passwordMaker/src/Swing/JFrame_.java
@@ -0,0 +1,14 @@
+package Swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class JFrame_ extends JFrame {
+
+    public void Creat(String title){
+
+    }
+
+}
